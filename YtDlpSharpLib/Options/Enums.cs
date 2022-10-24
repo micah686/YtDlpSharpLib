4bbbdf95f73e1960be12be249076e7ad2363fe3d
Micah <micah686@github.com>
2022-10-23 23:26:06 -0700

    Updated Options Namespaces

--- YtDlpSharpLib/Options/Enums.cs
+++ YtDlpSharpLib/Options/Enums.cs
@@ -1,29 +1,28 @@
 ﻿
-namespace YtDlpSharpLib.Options
+namespace YtDlpSharpLib.Options;
+
+/// <summary>
+/// Possible video merging formats.
+/// </summary>
+public enum DownloadMergeFormat
 {
-    /// <summary>
-    /// Possible video merging formats.
-    /// </summary>
-    public enum DownloadMergeFormat
-    {
-        Unspecified, Avi, Flv, Mkv, Mov, Mp4, Webm
-    }
+    Unspecified, Avi, Flv, Mkv, Mov, Mp4, Webm
+}
 
-    /// <summary>
-    /// Possible audio formats for audio conversion.
-    /// </summary>
-    public enum AudioConversionFormat
-    {
-        //Best, Aac, Flac, Mp3, M4a, Opus, Vorbis, Wav
-        Best, Aac, Alac, Flac, M4a, Mp3, Opus, Vorbis, Wav
-    }
+/// <summary>
+/// Possible audio formats for audio conversion.
+/// </summary>
+public enum AudioConversionFormat
+{
+    //Best, Aac, Flac, Mp3, M4a, Opus, Vorbis, Wav
+    Best, Aac, Alac, Flac, M4a, Mp3, Opus, Vorbis, Wav
+}
 
-    /// <summary>
-    /// Possible video formats for video conversion.
-    /// </summary>
-    public enum VideoRecodeFormat
-    {
-        //None, Mp4, Mkv, Ogg, Webm, Flv, Avi
-        None, Avi, Flv, Mkv, Mov, Mp4, Webm, Aac, Aiff, Alac, Flac, M4a, Mka, Mp3, Ogg, Opus, Vorbis, Wav
-    }
+/// <summary>
+/// Possible video formats for video conversion.
+/// </summary>
+public enum VideoRecodeFormat
+{
+    //None, Mp4, Mkv, Ogg, Webm, Flv, Avi
+    None, Avi, Flv, Mkv, Mov, Mp4, Webm, Aac, Aiff, Alac, Flac, M4a, Mka, Mp3, Ogg, Opus, Vorbis, Wav
 }
--- YtDlpSharpLib/Options/OptionSet.Authentication.cs
+++ YtDlpSharpLib/Options/OptionSet.Authentication.cs
@@ -1,84 +1,83 @@
-﻿namespace YtDlpSharpLib.Options
-{
-    public partial class OptionSet
-    {
-        private Option<string> _username = new Option<string>("-u", "--username");
-        private Option<string> _password = new Option<string>("-p", "--password");
-        private Option<string> _twofactor = new Option<string>("-2", "--twofactor");
-        private Option<bool> _netrc = new Option<bool>("-n", "--netrc");
-        private Option<string> _netrcLocation = new Option<string>("--netrc-location");
-        private Option<string> _videoPassword = new Option<string>("--video-password");
-        private Option<string> _apMso = new Option<string>("--ap-mso");
-        private Option<string> _apUsername = new Option<string>("--ap-username");
-        private Option<string> _apPassword = new Option<string>("--ap-password");
-        private Option<bool> _apListMso = new Option<bool>("--ap-list-mso");
-        private Option<string> _clientCertificate = new Option<string>("--client-certificate");
-        private Option<string> _clientCertificateKey = new Option<string>("--client-certificate-key");
-        private Option<string> _ClientCertificatePassword = new Option<string>("--client-certificate-password");
+﻿namespace YtDlpSharpLib.Options;
 
-        /// <summary>
-        /// Login with this account ID
-        /// </summary>
-        public string Username { get => _username.Value; set => _username.Value = value; }
-        /// <summary>
-        /// Account password. If this option is left
-        /// out, yt-dlp will ask interactively
-        /// </summary>
-        public string Password { get => _password.Value; set => _password.Value = value; }
-        /// <summary>
-        /// Two-factor authentication code
-        /// </summary>
-        public string TwoFactor { get => _twofactor.Value; set => _twofactor.Value = value; }
-        /// <summary>
-        /// Use .netrc authentication data
-        /// </summary>
-        public bool Netrc { get => _netrc.Value; set => _netrc.Value = value; }
-        /// <summary>
-        /// Location of .netrc authentication data;
-        /// either the path or its containing directory.
-        /// Defaults to ~/.netrc
-        /// </summary>
-        public string NetrcLocation { get => _netrcLocation.Value; set => _netrcLocation.Value = value; }
-        /// <summary>
-        /// Video password (vimeo, youku)
-        /// </summary>
-        public string VideoPassword { get => _videoPassword.Value; set => _videoPassword.Value = value; }
-        /// <summary>
-        /// Adobe Pass multiple-system operator (TV
-        /// provider) identifier, use --ap-list-mso
-        /// for a list of available MSOs
-        /// </summary>
-        public string ApMso { get => _apMso.Value; set => _apMso.Value = value; }
-        /// <summary>
-        /// Multiple-system operator account login
-        /// </summary>
-        public string ApUsername { get => _apUsername.Value; set => _apUsername.Value = value; }
-        /// <summary>
-        /// Multiple-system operator account
-        /// password. If this option is left out,
-        /// yt-dlp will ask interactively.
-        /// </summary>
-        public string ApPassword { get => _apPassword.Value; set => _apPassword.Value = value; }
-        /// <summary>
-        /// List all supported multiple-system
-        /// operators
-        /// </summary>
-        public bool ApListMso { get => _apListMso.Value; set => _apListMso.Value = value; }
-        /// <summary>
-        /// Path to client certificate file in PEM
-        /// format. May include the private key
-        /// </summary>
-        public string ClientCertificate { get => _clientCertificate.Value; set => _clientCertificate.Value = value; }
-        /// <summary>
-        /// Path to private key file for client
-        /// certificate
-        /// </summary>
-        public string ClientCertificateKey { get => _clientCertificateKey.Value; set => _clientCertificateKey.Value = value; }
-        /// <summary>
-        /// Password for client certificate private key,
-        /// if encrypted. If not provided, and the key
-        /// is encrypted, yt-dlp will ask interactively
-        /// </summary>
-        public string ClientCertificatePassword { get => _ClientCertificatePassword.Value;  set => _ClientCertificatePassword.Value = value; }
-    }
+public partial class OptionSet
+{
+    private Option<string> _username = new Option<string>("-u", "--username");
+    private Option<string> _password = new Option<string>("-p", "--password");
+    private Option<string> _twofactor = new Option<string>("-2", "--twofactor");
+    private Option<bool> _netrc = new Option<bool>("-n", "--netrc");
+    private Option<string> _netrcLocation = new Option<string>("--netrc-location");
+    private Option<string> _videoPassword = new Option<string>("--video-password");
+    private Option<string> _apMso = new Option<string>("--ap-mso");
+    private Option<string> _apUsername = new Option<string>("--ap-username");
+    private Option<string> _apPassword = new Option<string>("--ap-password");
+    private Option<bool> _apListMso = new Option<bool>("--ap-list-mso");
+    private Option<string> _clientCertificate = new Option<string>("--client-certificate");
+    private Option<string> _clientCertificateKey = new Option<string>("--client-certificate-key");
+    private Option<string> _ClientCertificatePassword = new Option<string>("--client-certificate-password");
+
+    /// <summary>
+    /// Login with this account ID
+    /// </summary>
+    public string Username { get => _username.Value; set => _username.Value = value; }
+    /// <summary>
+    /// Account password. If this option is left
+    /// out, yt-dlp will ask interactively
+    /// </summary>
+    public string Password { get => _password.Value; set => _password.Value = value; }
+    /// <summary>
+    /// Two-factor authentication code
+    /// </summary>
+    public string TwoFactor { get => _twofactor.Value; set => _twofactor.Value = value; }
+    /// <summary>
+    /// Use .netrc authentication data
+    /// </summary>
+    public bool Netrc { get => _netrc.Value; set => _netrc.Value = value; }
+    /// <summary>
+    /// Location of .netrc authentication data;
+    /// either the path or its containing directory.
+    /// Defaults to ~/.netrc
+    /// </summary>
+    public string NetrcLocation { get => _netrcLocation.Value; set => _netrcLocation.Value = value; }
+    /// <summary>
+    /// Video password (vimeo, youku)
+    /// </summary>
+    public string VideoPassword { get => _videoPassword.Value; set => _videoPassword.Value = value; }
+    /// <summary>
+    /// Adobe Pass multiple-system operator (TV
+    /// provider) identifier, use --ap-list-mso
+    /// for a list of available MSOs
+    /// </summary>
+    public string ApMso { get => _apMso.Value; set => _apMso.Value = value; }
+    /// <summary>
+    /// Multiple-system operator account login
+    /// </summary>
+    public string ApUsername { get => _apUsername.Value; set => _apUsername.Value = value; }
+    /// <summary>
+    /// Multiple-system operator account
+    /// password. If this option is left out,
+    /// yt-dlp will ask interactively.
+    /// </summary>
+    public string ApPassword { get => _apPassword.Value; set => _apPassword.Value = value; }
+    /// <summary>
+    /// List all supported multiple-system
+    /// operators
+    /// </summary>
+    public bool ApListMso { get => _apListMso.Value; set => _apListMso.Value = value; }
+    /// <summary>
+    /// Path to client certificate file in PEM
+    /// format. May include the private key
+    /// </summary>
+    public string ClientCertificate { get => _clientCertificate.Value; set => _clientCertificate.Value = value; }
+    /// <summary>
+    /// Path to private key file for client
+    /// certificate
+    /// </summary>
+    public string ClientCertificateKey { get => _clientCertificateKey.Value; set => _clientCertificateKey.Value = value; }
+    /// <summary>
+    /// Password for client certificate private key,
+    /// if encrypted. If not provided, and the key
+    /// is encrypted, yt-dlp will ask interactively
+    /// </summary>
+    public string ClientCertificatePassword { get => _ClientCertificatePassword.Value;  set => _ClientCertificatePassword.Value = value; }
 }
